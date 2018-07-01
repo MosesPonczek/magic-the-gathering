6993a40855e245426d4c63bfc6e2ca3f20cd2004
przemyslawdrozniak@colorvision.pl <przemyslawdrozniak@colorvision.pl>
2018-07-02 00:00:42 +0200

    Create script with api

--- src/js/axios.js
+++ src/js/axios.js
@@ -1,10 +1,57 @@
 // Axios
 import axios from 'axios';
 
-axios.get('https://swapi.co/api/people/1/')
+axios.get('https://api.magicthegathering.io/v1/cards')
     .then(res => res.data)
     .then(data => {
-        console.log(data);
+
+        const getCardButton = document.querySelector('.get-card-button');
+
+        const card = data.cards[Math.floor(Math.random() * data.cards.length)];
+
+        const cardsListElem = document.querySelector('.cards');
+        const cardsItemElem = document.createElement('div');
+        cardsItemElem.classList.add('cards__item');
+
+        const cardImageElem = document.createElement('img');
+        cardImageElem.classList.add('cards__image');
+        cardImageElem.src = card.imageUrl;
+
+        const cardName = card.name;
+        const cardTitleElem = document.createElement('h2');
+        cardTitleElem.classList.add('cards__title');
+        cardTitleElem.innerHTML = `Nazwa karty: <span>${cardName}</span>`;
+
+        const manaCost = card.manaCost;
+        const cardManaElem = document.createElement('small');
+        cardManaElem.classList.add('cards__mana-cost');
+        cardManaElem.innerHTML = `Użycie many: ${manaCost}`;
+
+        const cardDescription = card.originalText;
+        const cardDescriptionElem = document.createElement('p');
+        cardDescriptionElem.classList.add('cards__description');
+        cardDescriptionElem.innerHTML = `Opis karty: ${cardDescription}`;
+
+        const closeButton = document.createElement('button');
+        closeButton.innerHTML = 'X';
+        closeButton.classList.add('close-button');
+
+
+        cardsItemElem.appendChild(cardImageElem);
+        cardsItemElem.appendChild(cardTitleElem);
+        cardsItemElem.appendChild(cardManaElem);
+        cardsItemElem.appendChild(cardDescriptionElem);
+        cardsItemElem.appendChild(closeButton);
+
+
+        getCardButton.addEventListener('click', function () {
+            cardsListElem.appendChild(cardsItemElem);
+        });
+        closeButton.addEventListener('click', function () {
+            cardsItemElem.remove();
+        });
+
+
     })
     .catch(error => {
         console.log(error);
